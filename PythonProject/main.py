bb8243308524c3032520e42e8f84f0f0eaf7a89a
Daniel <kotvesopogah@gmail.com>
2025-08-28 23:19:06 +0300

    -

---
+++ PythonProject/main.py
@@ -0,0 +1,15 @@
+from pyrogram import Client, filters
+
+api_id = 27960228
+api_hash = "b8c3132ab61115b00ba2a90974b5cc53"
+
+app = Client("my_account", api_id=api_id, api_hash=api_hash)
+
+SOURCE_CHAT_ID = -4982847299
+DEST_CHAT_ID = -4905406251
+
+@app.on_message(filters.chat(SOURCE_CHAT_ID))
+async def get_chat_id(client, message):
+    await message.forward(chat_id=DEST_CHAT_ID)
+
+app.run()
